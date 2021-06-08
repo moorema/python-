5bbc1e47f598a022bb8eac2bd08e235f74fe14dc
马杰 <32790758+moorema@users.noreply.github.com>
2021-06-08 11:47:04 +0800

    Update 爬虫.py
    学习爬虫

--- 爬虫.py
+++ 爬虫.py
@@ -1,46 +1,45 @@
-# import requests
-# 
-# 
-# url = 'https://www.sogou.com/web?query=周杰伦'
-# dic = {
-#     'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.3'
-# }
-# resp = requests.get(url, headers=dic)
-# 
-# print(resp.text)
+import requests
+
+url = 'https://www.sogou.com/web?query=周杰伦'
+dic = {
+    'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.3'
+}
+resp = requests.get(url, headers=dic)
+
+print(resp.text)
 ---
 # 河南采购,cookie手动反反爬(半成品手动cookie会过期)
-# import requests
-# 
-# url = "http://www.hngp.gov.cn/henan/search?&"
-# ua = {
-#     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
-#                   'Chrome/91.0.4472.77 Safari/537.3',
-# 
-# }
-# dc = input('请输入查询的关键字: ')
-# kw = {
-#     'ctk': 'eaa01c88ee244aee9f49abd4892654f1',
-#     "q": dc,
-# }
-# cookie = {
-#     'sId': 'eaa01c88ee244aee9f49abd4892654f1'
-# }
-# resp = requests.get(url=url, headers=ua, params=kw, cookies=cookie)
-# print(resp.text)
-# resp.close()
+import requests
+
+url = "http://www.hngp.gov.cn/henan/search?&"
+ua = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                  'Chrome/91.0.4472.77 Safari/537.3',
+
+}
+dc = input('请输入查询的关键字: ')
+kw = {
+    'ctk': 'eaa01c88ee244aee9f49abd4892654f1',
+    "q": dc,
+}
+cookie = {
+    'sId': 'eaa01c88ee244aee9f49abd4892654f1'
+}
+resp = requests.get(url=url, headers=ua, params=kw, cookies=cookie)
+print(resp.text)
+resp.close()
 ---
 # 正则表达式
 import re
 
-# # findall:匹配所有符合正则的内容
-# lst = re.findall(r"\w+", "我的电环号码是:10086, 我女朋友的电话号码是10000")
-# print(lst)
-#
-# # finditer: 匹配字符串中所哟有的内容,返回的迭代器
-# it = re.finditer(r"\d+", "我的电环号码是:10086, 我女朋友的电话号码是10000")
-# for i in it:
-#     print(i.group())
+# findall:匹配所有符合正则的内容
+lst = re.findall(r"\w+", "我的电环号码是:10086, 我女朋友的电话号码是10000")
+print(lst)
+
+# finditer: 匹配字符串中所哟有的内容,返回的迭代器
+it = re.finditer(r"\d+", "我的电环号码是:10086, 我女朋友的电话号码是10000")
+for i in it:
+    print(i.group())
 
 s = """
 <div class='jay'><span id='1'>法师</span></div>
